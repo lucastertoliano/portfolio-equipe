9eba81664cf3f06668c62832157c2ac1e828ec37
unknown <pedrinho.jpln@gmail.com>
2025-08-30 23:42:24 -0300

    fiz toda a parte do forms

--- script.js
+++ script.js
@@ -6,4 +6,22 @@ function showSidebar() {
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
-}     
+}  
+document.getElementById('form-contato').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let nomeDigitado = document.getElementById('nome').value.trim();
+    let emailDigitado = document.getElementById('email').value.trim().toLowerCase();
+    let mensagemDigitado = document.getElementById('mensagem').value.trim();
+
+    if(nomeDigitado.split(' ').length < 2){
+        alert('Digite seu nome completo.');
+        return;
+    }
+    if(mensagemDigitado === ''){
+        alert('Digite sua mensagem.');
+        return;
+    }
+    
+    alert('Mensagem enviada com sucesso!');
+});
