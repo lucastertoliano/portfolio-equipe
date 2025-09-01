f86a7fe63a29f14b4f705d07e2cce3dd1590473d
lucastertoliano <158801827+lucastertoliano@users.noreply.github.com>
2025-09-01 17:57:56 -0300

    update varias seções e add validação do form com innerHTML

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Menu Hamburguer (Apenas Mobile)
+
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -6,29 +8,54 @@ function showSidebar() {
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
-}  
-document.getElementById('form-contato').addEventListener('submit', function(e) {
+} 
+
+
+// Validação do Formulário de Contato
+
+const formContato = document.getElementById('form-contato');
+const nomeInput = document.getElementById('nome');
+const emailInput = document.getElementById('email');
+const mensagemInput = document.getElementById('mensagem');
+const mensagemForm = document.getElementById('mensagem-form');
+
+formContato.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    let nomeDigitado = document.getElementById('nome').value.trim();
-    let emailDigitado = document.getElementById('email').value.trim().toLowerCase();
-    let mensagemDigitado = document.getElementById('mensagem').value.trim();
+    // valida se o nome tem pelo menos duas palavras e se não está vazio
+    if (nomeInput.value.trim().split(" ").length < 2) {
+        mensagemForm.innerHTML = "Digite seu nome completo.";
+        mensagemForm.style.backgroundColor = "#f8d7da";
+        mensagemForm.style.color = "#721c24";
+        mensagemForm.style.border = "1px solid #f5c6cb";
+        mensagemForm.style.padding = "10px";
+        mensagemForm.style.borderRadius = "5px";
+        return;
+    }
 
-    if(nomeDigitado.split(' ').length < 2){
-        alert('Digite seu nome completo.');
-        return;
+    // valida se o email é válido atraves de regex
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value.trim())) {
+    mensagemForm.innerHTML = "Digite um email válido.";
+    mensagemForm.style.backgroundColor = "#f8d7da";
+    mensagemForm.style.color = "#721c24";
+    mensagemForm.style.border = "1px solid #f5c6cb";
+    mensagemForm.style.padding = "10px";
+    mensagemForm.style.borderRadius = "5px";
+    return;
     }
-    if(mensagemDigitado === ''){
-        alert('Digite sua mensagem.');
-        return;
-    }
-    
-    alert('Mensagem enviada com sucesso!');
+
+    // mensagem de sucesso
+    mensagemForm.innerHTML = "Mensagem enviada com sucesso!";
+    mensagemForm.style.backgroundColor = "#d4edda";
+    mensagemForm.style.color = "#155724";
+    mensagemForm.style.border = "1px solid #c3e6cb";
+    mensagemForm.style.padding = "10px";
+    mensagemForm.style.borderRadius = "5px";
+    formContato.reset();
+
 });
 
-// =================================
-// BOTÃO VOLTAR AO TOPO
-// =================================
+// Botão "Voltar ao Topo"
 
 function scrollToTop() {
     window.scrollTo({
